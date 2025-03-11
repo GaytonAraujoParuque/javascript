0fb285012fa755ba1c53929e85660ba90b49a473
GaytonAraujoParuque <gaytonparuque2@gmail.com>
2025-03-11 13:56:21 +0200

    Atualizacao

--- curso/aula14ex/ex016/script.js
+++ curso/aula14ex/ex016/script.js
@@ -4,13 +4,26 @@ function contar(){
     var inicio = window.document.querySelector('#inc')
     var  fim = window.document.querySelector('#fim')
     var passo = window.document.querySelector('#passo')
+    var res = window.document.getElementById('res')
     inicio = Number(inicio.value)
-    fim = Number.parseInt(fim.value)
-    passo = Number.parseInt(passo.value)
+    fim = Number(fim.value)
+    passo = Number(passo.value)
 
-    for (var c = inicio; c <= fim; c = c + passo){
-        
+    /*if (inicio.length == 0 || fim.length == 0 || passo.length == 'undefined'){
+        window.alert("Por favor, preencha todos os campos")
+    }else{
+        alert(`inicio = ${inicio} fim = ${fim.length} passo = ${passo.length}`)
+    }*/
+    res.innerHTML = 'Contando... <br>'
+    if (inicio > fim){
+        for (var c = inicio; c >=fim; c -= passo){
+            res.innerText += `${c} `
+        }
+    }else{
+        for (c = inicio; c <= fim; c += passo){
+            res.innerHTML += `${c}`
+        }
     }
-        
 
+    res.innerHTML += `Fim`
 }
